802c7fc58e1774c7e1c568d0242b94af2eef6d26
Tim <tspalmieri2018@gmail.com>
2020-02-18 17:55:25 -0500

    some more stuff

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,4 +1,38 @@
 # app/robo_advisor.py
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+print("REQUESTING SOME DATA FROM THE INTERNET...")
+
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default = "OOPS")
+symbol = "TSLA"
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}"
+print("URL:", request_url)
+
+response = requests.get(request_url)
+print(type(response))
+#print(dir(response))
+print(response.status_code)
+#print(response.text)
+print(type(response.text))
+
+parsed_response = json.loads(response.text)
+print(type(parsed_response)) #> list
+
+print(parsed_response)
+
+# for any_prod in parsed_response:
+#     print(any_prod["id"], any_prod["name"])
+# first_prod = parsed_response[0]
+
+# print(first_prod["name"])
+
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
