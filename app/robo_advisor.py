b1a7d3252f338f8d45cc38e22ba46dfc09bd2416
Tim <tspalmieri2018@gmail.com>
2020-02-23 21:57:51 -0500

    recent high finished

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -18,7 +18,38 @@ response = requests.get(request_url)
 parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
-latest_close = parsed_response["Time Series (Daily)"][f"{last_refreshed}"]["4. close"]
+
+tsd = parsed_response["Time Series (Daily)"]
+dates = list(tsd.keys())
+
+latest_day = dates[0]
+
+###########################
+#   Getting latest close  #
+###########################
+
+latest_close = parsed_response["Time Series (Daily)"][f"{latest_day}"]["4. close"]
+
+
+
+###########################
+#    Getting recent high  #
+###########################
+
+high_prices = []
+
+for i in dates:
+    high_prices.append(parsed_response["Time Series (Daily)"][f"{str(i)}"]["2. high"])
+
+recent_high = max(high_prices)
+
+
+###########################
+#    Getting recent low  #
+###########################
+
+recent_low = parsed_response["Time Series (Daily)"][f"{last_refreshed}"]["3. low"]
+
 
 print(datetime.today())
 
@@ -32,8 +63,8 @@ print(f"REQUEST AT: {str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))}")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
-print("RECENT HIGH: $101,000.00")
-print("RECENT LOW: $99,000.00")
+print(f"RECENT HIGH: {to_usd(float(recent_high))}")
+print(f"RECENT LOW: {to_usd(float(recent_low))}")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
